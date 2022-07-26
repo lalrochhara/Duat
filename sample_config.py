fef192a0bd889d09baf341c68d240bfb5088c21b
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2022-07-26 11:54:50 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -27,20 +27,21 @@ if HEROKU:
     RSS_DELAY = int(environ.get("RSS_DELAY", None))
     PM_PERMIT = bool(int(environ.get("PM_PERMIT", None)))
 else:
-    BOT_TOKEN = "5570459431:AAFkMKp9W4AOaUyvu8_YbpZxY0C4b0slyIQ"
-    API_ID ="2370938"
-    API_HASH = "28ea568cb7ac6cbf1291799e0e2c77d2"
-    USERBOT_PREFIX = ".sh"
-    PHONE_NUMBER = "+916909292139"  # Need for Userbot
+    BOT_TOKEN = "467677575:YZfaakjwd545dfg-N6JStihhuw5gQeZHntc"
+    API_ID = 123456
+    API_HASH = "dfxcgs5s12hdcxfgdfz"
+    USERBOT_PREFIX = "."
+    PHONE_NUMBER = "+916969696969"  # Need for Userbot
     SUDO_USERS_ID = [
-        1382148141,
+        4543744343,
+        543214651351,
     ]  # Sudo users have full access to everything, don't trust anyone
-    LOG_GROUP_ID = -1001727551382
-    GBAN_LOG_GROUP_ID = -628808052
-    MESSAGE_DUMP_CHAT = -1001727551382
+    LOG_GROUP_ID = -100125431255
+    GBAN_LOG_GROUP_ID = -100125431255
+    MESSAGE_DUMP_CHAT = -1001181696437
     WELCOME_DELAY_KICK_SEC = 300
-    MONGO_URL = "mongodb+srv://sangteiubot:sangteiubot@cluster0.hnd2z.mongodb.net/?retryWrites=true&w=majority"
-    ARQ_API_KEY = "HAGPNZ-MXGLCP-JEFIAL-XVBZJG-ARQ"
+    MONGO_URL = "mongodb+srv://username:password@cluster0.ksiis.mongodb.net/YourDataBaseName?retryWrites=true&w=majority"
+    ARQ_API_KEY = "Get this from @ARQRobot"
     ARQ_API_URL = "https://arq.hamker.in"
     LOG_MENTIONS = True
     RSS_DELAY = 300  # In seconds
